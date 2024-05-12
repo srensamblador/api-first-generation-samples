dd5b901c22bb583f8ba6cb4fd8b40ff404893106
srensamblador <uo238524@uniovi.es>
2024-05-12 20:32:26 +0200

    feat: expand sample API a little

--- code/api-first-grpc/src/main/java/com/srensamblador/apifirst/grpc/controller/PetController.java
+++ code/api-first-grpc/src/main/java/com/srensamblador/apifirst/grpc/controller/PetController.java
@@ -1,17 +1,39 @@
 package com.srensamblador.apifirst.grpc.controller;
 
-import com.srensamblador.apifirst.grpc.petstore.PetProto;
-import com.srensamblador.apifirst.grpc.petstore.PetServiceProto;
-import com.srensamblador.apifirst.grpc.petstore.PetStoreServiceGrpc;
-import com.srensamblador.apifirst.grpc.petstore.request.GetPetRequestProto;
+import com.google.protobuf.Empty;
+import com.srensamblador.apifirst.grpc.petstore.v1.PetProto;
+import com.srensamblador.apifirst.grpc.petstore.v1.PetStoreServiceGrpc;
+import com.srensamblador.apifirst.grpc.petstore.v1.request.CreatePetRequestProto;
+import com.srensamblador.apifirst.grpc.petstore.v1.request.DeletePetRequestProto;
+import com.srensamblador.apifirst.grpc.petstore.v1.request.GetPetRequestProto;
+import com.srensamblador.apifirst.grpc.petstore.v1.request.UpdatePetRequestProto;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
 public class PetController extends PetStoreServiceGrpc.PetStoreServiceImplBase {
+    @Override
+    public void get(GetPetRequestProto.GetPetRequest request, StreamObserver<PetProto.Pet> responseObserver) {
+        super.get(request, responseObserver);
+    }
 
     @Override
-    public void petById(GetPetRequestProto.GetPetRequest request, StreamObserver<PetProto.Pet> responseObserver) {
-        super.petById(request, responseObserver);
+    public void list(Empty request, StreamObserver<PetProto.Pet> responseObserver) {
+        super.list(request, responseObserver);
+    }
+
+    @Override
+    public void create(CreatePetRequestProto.CreatePetRequest request, StreamObserver<PetProto.Pet> responseObserver) {
+        super.create(request, responseObserver);
+    }
+
+    @Override
+    public void delete(DeletePetRequestProto.DeletePetRequest request, StreamObserver<Empty> responseObserver) {
+        super.delete(request, responseObserver);
+    }
+
+    @Override
+    public void update(UpdatePetRequestProto.UpdatePetRequest request, StreamObserver<Empty> responseObserver) {
+        super.update(request, responseObserver);
     }
 }
